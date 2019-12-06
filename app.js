3a0ea1436e44be405ce725834f2aab11749ec3e4
stormkirill98 <stormkirill98@gmail.com>
2019-12-06 16:39:39 +0300

    add bodyParser and cookiePArser

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
 const app = express();
+
 app.use(cors());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.all('/', (req, res) => {
     res.end('Hello World')
