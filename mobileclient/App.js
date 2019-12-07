3568fe77b6354d4ff5648f26302b9e7ea8a50c8a
kamych1916 <kamych1916@gmail.com>
2019-12-07 21:24:14 +0300

    test

--- mobileclient/App.js
+++ mobileclient/App.js
@@ -3,7 +3,6 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Products from './components/productscomp';
 import Purchase from './components/purchase';
-import Notes from './components/notes';
 import { StatusBar} from 'react-native';
 
 
@@ -13,7 +12,6 @@ console.disableYellowBox = true;
 const MainNavigator = createStackNavigator({
   Purchase: {screen: Purchase},
   Products: {screen: Products},
-  Notes: {screen: Notes},
 });
 
 const App = createAppContainer(MainNavigator);
